e7e1531f07a8f007eec93002a2e1478b1ef4f8b9
Zeinab Mohamed <zm4816035@gmail.com>
2023-06-12 22:18:03 +0300

    initial commit

---
+++ SimpleMoviesApp/MovieImagesVC.swift
@@ -0,0 +1,30 @@
+//
+//  MovieImagesVC.swift
+//  SimpleMoviesApp
+//
+//  Created by Zeinab on 11/06/2023.
+//
+
+import UIKit
+
+class MovieImagesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
